d7351094a2978f02b4ff5f505f8993a0796c1115
kcodes57 <kir652986@gmail.com>
2024-08-09 18:35:25 -0500

    styled basic structure

---
+++ src/style.cs
@@ -0,0 +1,19 @@
+body {
+  background-color: #f9f7fe;
+  font-family: Helvetica, Arial, sans-serif;
+}
+.container {
+  margin: 120px auto;
+  max-width: 600px;
+  background-color: rgb(255, 255, 255);
+}
+header {
+  margin-bottom: 30px;
+}
+h1 {
+  font-weight: 800;
+  font-size: 30px;
+  line-height: 1.5;
+  text-align: center;
+  color: #272044;
+}
