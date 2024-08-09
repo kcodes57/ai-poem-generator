69b918d9b310af28d7063c174ac92d9d259a6052
kcodes57 <kir652986@gmail.com>
2024-08-09 18:59:24 -0500

    styled form

--- src/style.cs
+++ src/style.cs
@@ -17,3 +17,33 @@ h1 {
   text-align: center;
   color: #272044;
 }
+form {
+  padding: 30px;
+  background-color: white;
+  border-radius: 10px;
+  display: flex;
+  box-shadow: 0px 20px 60px rgba(65, 50, 100, 0.08);
+  margin-bottom: 30px;
+}
+
+.instructions {
+  padding: 16px;
+  border: 1px solid gray;
+  margin: 10px;
+  border-radius: 3px;
+  font-size: 15px;
+  width: 60%;
+  line-height: 20px;
+  color: #272044;
+}
+.submit {
+  padding: 10px;
+  margin: 15px;
+  background-color: #885df1;
+  color: whitesmoke;
+  border-radius: 5px;
+  margin-left: 10px;
+  border: none;
+  padding: 15px;
+  font-size: 15px;
+}
