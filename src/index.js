0b514da091d103a03135d8a8b0b23e5cdb326672
kcodes57 <kir652986@gmail.com>
2024-08-11 18:59:52 -0500

    created function to insert poem onto page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  let poemElement = document.querySelector("#poem");
+  new Typewriter("#poem", {
+    strings: "To keep your marriage brimming",
+    autoStart: true,
+    cursor: "",
+    delay: 1,
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
