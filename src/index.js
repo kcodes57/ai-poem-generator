c47eb9249925cc1955fc22ab2941173c7eee4c7e
kcodes57 <kir652986@gmail.com>
2024-08-12 16:42:24 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let apiKey = "441fcacb9dt020b37114da0ba3e3f5of";
   let prompt = `User Instructions: Please generate a short poem about ${userInstructions.value}`;
   let context =
-    "You are a poem writer and love to write short poems. Please follow user instructions. The poem should be 4 lines in length in basic html, each seperated by a </br>. Do not have more than one line of spacing between each line of the poem. Sign the poem with 'SheCodes AI' in a <strong></strong> element";
+    "You are a poem writer and love to write short poems. Please follow user instructions. The poem should be 4 lines in length in basic html, each seperated by a </br>. Do not have more than one line of spacing between each line of the poem.  Sign the poem with 'SheCodes AI' in a <strong></strong> element";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(displayPoem);
 }
