a1caf74bb87a688a97aebae0d15f5cc404ec4d85
kcodes57 <kir652986@gmail.com>
2024-08-20 11:40:32 -0500

    updated context to fix spacing issue

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ function generatePoem(event) {
   let apiKey = "441fcacb9dt020b37114da0ba3e3f5of";
   let prompt = `User Instructions: Please generate a short poem about ${userInstructions.value}`;
   let context =
-    "You are a poem writer and love to write short poems. Please follow user instructions. The poem should be 4 lines in length in basic html, each separated by a </br>. Do not include a title. Do not have more than one line of spacing between each line of the poem.  Sign the bottom of poem with 'SheCodes AI' in a <strong></strong> element";
+    "You are a poem writer and love to write short poems. Please follow user instructions. The poem should be 4 lines in length in basic html. The 4 lines should be inside a single <p> element and each line should be separated by a <br /> element. Do not include a title. Do not have more than one line of spacing between each line of the poem.  Sign the bottom of poem with 'SheCodes AI' in a <strong></strong> element.";
+
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
